eb8d00ec9e4214724083fc88909d623ed6f3b8a5
xan335 <hodievhursedhon@gmail.com>
2024-11-04 17:25:45 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let one = document.querySelector('#one');
+let inc = document.querySelector('#inc')
+let dec = document.querySelector('#dec')
+let rnd = document.querySelector('#rnd')
+let resed = document.querySelector('#resed')
+
+let rt = 0;
+let rt1 = 0;
+
+one.textContent = 0;
+
+rnd.onclick = () => {
+    one.textContent = Math.floor(Math.random() * 100);
+    rt = one.textContent
+}
+inc.onclick = () => {
+    one.textContent = ++rt;
+}
+dec.onclick = () => {
+    one.textContent = --rt;
+}
+
+resed.onclick = () => {
+    one.textContent = (rt = 0);
+}
+
+
